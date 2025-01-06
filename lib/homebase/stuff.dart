c4c4418220c16aabc94769d0ba559b26f98eaa4a
Eight Rice <astmobronsita@gmail.com>
2025-01-06 14:14:08 +0200

    switching to indexer

---
+++ lib/homebase/stuff.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class AllDaos extends StatefulWidget {
+  const AllDaos({super.key});
+
+  @override
+  State<AllDaos> createState() => _AllDaosState();
+}
+
+class _AllDaosState extends State<AllDaos> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+
+class Dcard extends StatelessWidget {
+  const Dcard({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
